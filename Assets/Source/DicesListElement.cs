dee0aeb8f5460971d35ef8f54be5856ae0ddff71
max-fluff <borshishe@gmail.com>
2024-07-07 21:34:07 +0300

    init list element value fix

--- Assets/Source/DicesListElement.cs
+++ Assets/Source/DicesListElement.cs
@@ -10,13 +10,13 @@ namespace Source
         private void Awake()
         {
             inputField.onEndEdit.AddListener(ProcessEndEdit);
-            inputField.SetTextWithoutNotify("6");
+            inputField.SetTextWithoutNotify("0");
         }
 
         private void ProcessEndEdit(string value)
         {
             if (string.IsNullOrEmpty(value))
-                inputField.SetTextWithoutNotify("6");
+                inputField.SetTextWithoutNotify("0");
             else
             {
                 var intValue = Mathf.Clamp(int.Parse(value), 0, 6);
